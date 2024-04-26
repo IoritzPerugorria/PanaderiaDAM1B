20d217c0702bb33accdb490f02d6e1b51b557e20
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-04-26 16:08:59 +0200

    1ª Reestructuracion Login.fxml

--- src/main/java/Controladores/ControladorLogin.java
+++ src/main/java/Controladores/ControladorLogin.java
@@ -1,4 +1,10 @@
 package Controladores;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ControladorLogin {
+    public Button btnLogin;
+    public TextField txtFldCredencial1;
+    public TextField txtFldCredencial2;
 }
