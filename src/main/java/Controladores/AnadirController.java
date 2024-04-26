038cc3fbfec1668e3f94c6f659dcb1b7af38eb25
OlgaAlfaro <oalfarolas1@educacion.navarra.es>
2024-04-26 16:53:53 +0200

    Comienzo desarrollo del controlador de la pantalla de Añadir

---
+++ src/main/java/Controladores/AnadirController.java
@@ -0,0 +1,18 @@
+package Controladores;
+
+import javafx.fxml.FXML;
+
+public class AnadirController {
+
+
+    @FXML
+    protected void anadirIngrediente(){
+
+    }
+
+    @FXML
+    protected void anadirProducto(){
+
+    }
+
+}
