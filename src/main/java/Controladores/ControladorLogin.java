32385e0108986c5f67faec09ca3e4f8fc505ad5a
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-04-29 16:20:18 +0200

    Adicion fichero ConexionBBDD.java. 3ª Reestructuración Login.fxml (Nuevos nodos + encapsulacion).

--- src/main/java/Controladores/ControladorLogin.java
+++ src/main/java/Controladores/ControladorLogin.java
@@ -5,8 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class ControladorLogin {
+    @FXML
+    private Text txtCredencialesIncorrectas;
+    @FXML
+    private Text txtUsrObligatorio;
+    @FXML
+    private Text txtCtrObligatorio;
     @FXML
     private TextField txtFldCredencial1;
     @FXML
@@ -15,6 +22,23 @@ public class ControladorLogin {
     private Button btnLogin;
 
     public ControladorLogin(){
-
+        txtFldCredencial1 = new TextField();
+        txtFldCredencial2 = new PasswordField();
+        txtCredencialesIncorrectas = new Text();
+        txtUsrObligatorio = new Text();
+        txtCtrObligatorio = new Text();
     }
+
+    public void acceso(){
+        if (txtFldCredencial1 == null || txtFldCredencial2 == null){
+
+        }
+    }
+
+    private void indicarCampoObligatorio(Text texto){
+        texto.setText("* Este campo es obligatorio");
+    }
+
+
+
 }
