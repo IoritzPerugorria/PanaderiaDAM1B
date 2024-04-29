a98147ab497194cecb3d883384b08552381c598a
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-04-29 15:26:30 +0200

    2ª Reestructuracion Login.fxml (asignación correcta de los IDs en los nodos)

--- src/main/java/Controladores/ControladorLogin.java
+++ src/main/java/Controladores/ControladorLogin.java
@@ -1,10 +1,20 @@
 package Controladores;
 
+
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class ControladorLogin {
-    public Button btnLogin;
-    public TextField txtFldCredencial1;
-    public TextField txtFldCredencial2;
+    @FXML
+    private TextField txtFldCredencial1;
+    @FXML
+    private PasswordField txtFldCredencial2;
+    @FXML
+    private Button btnLogin;
+
+    public ControladorLogin(){
+
+    }
 }
