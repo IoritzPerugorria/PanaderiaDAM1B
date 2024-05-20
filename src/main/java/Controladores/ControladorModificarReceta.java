b21abd45c989c60019696bb63e4c703c7765c48f
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-05-20 16:56:50 +0200

    ControladorModificarReceta.java -> métodoInitialize(): Implementado código para detectar y responder en base a los objetos seleccionados.

--- src/main/java/Controladores/ControladorModificarReceta.java
+++ src/main/java/Controladores/ControladorModificarReceta.java
@@ -34,17 +34,45 @@ public class ControladorModificarReceta implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Para mostrar las recetas de la Base de Datos
         try {
             conexion = ConexionBBDD.conectar(conexion);
             PreparedStatement recogidaNombresRecetas = conexion.prepareStatement("SELECT NOMBRE FROM PRODUCTOS");
-            ResultSet resultado = recogidaNombresRecetas.executeQuery();
+            ResultSet resultadoNombres = recogidaNombresRecetas.executeQuery();
             ArrayList<String> listaNombresRecetas = new ArrayList<>();
-            while (resultado.next()){
-                listaNombresRecetas.add(resultado.getString("NOMBRE"));
+            while (resultadoNombres.next()){
+                listaNombresRecetas.add(resultadoNombres.getString("NOMBRE"));
             }
             for (int i = 0; i < listaNombresRecetas.size(); i++) {
                 listaRecetas.getItems().add(listaNombresRecetas.get(i));
             }
+
+        //Para detectar y responder en base a los objetos seleccionados
+            listaRecetas.setOnMouseClicked(event -> {
+                // Obtener el artículo seleccionado
+                Object selectedItem = listaRecetas.getSelectionModel().getSelectedItem();
+
+                // Acceder a los valores del elemento seleccionado y visualizarlos
+                if (selectedItem != null) {
+                    // Assuming the selected item is of type String
+                    String selectedValue = (String) selectedItem;
+
+                    // Buscar dicho ingrediente:
+                    try {
+                        PreparedStatement datosRecogidosRecetas = conexion.prepareStatement("SELECT * FROM PRODUCTOS WHERE NOMBRE = ?");
+                        datosRecogidosRecetas.setString(1, selectedValue);
+                        ResultSet resultadoDatos = datosRecogidosRecetas.executeQuery();
+                        resultadoDatos.next();
+                        nombreReceta.setText(resultadoDatos.getString("NOMBRE"));
+                        precioReceta.setText(resultadoDatos.getString("PRECIO"));
+
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Valor seleccionado: " + selectedValue);
+                }
+            });
+
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
