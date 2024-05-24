4b5ef6fa86676fb376aaba8fe56f749f77f91e41
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-05-24 16:10:18 +0200

    ControladorModificarReceta.java: Cambio dinamico de datos a modificar segun su selección completada.

--- src/main/java/Controladores/ControladorModificarReceta.java
+++ src/main/java/Controladores/ControladorModificarReceta.java
@@ -4,12 +4,8 @@ import BBDD.ConexionBBDD;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.sql.*;
@@ -27,13 +23,16 @@ public class ControladorModificarReceta implements Initializable {
     @FXML
     private TextField precioReceta;
     @FXML
-    private TextField TxtfldCantidadIngrediente;
-    @FXML
-    private VBox ingredientesRecetas;
-    private ArrayList<HBox>ingredientes;
+    private TextField txtfldCantidadIngrediente;
+    private int ID_ING;
+    private int ID_PR;
     Connection conexion;
-    String nombre;
 
+    public ControladorModificarReceta(){
+        nombreReceta = new TextField();
+        precioReceta = new TextField();
+        txtfldCantidadIngrediente = new TextField();
+    }
 
 
     public void modificarReceta(ActionEvent actionEvent) {
@@ -58,6 +57,7 @@ public class ControladorModificarReceta implements Initializable {
                 if ( recetaSeleccionada != null ) {
                     // Asumiendo que el Item seleccionado es del tipo String
                     String valorRecetaSeleccionada = (String) recetaSeleccionada;
+                    txtfldCantidadIngrediente.setText("");
 
                     // Buscar dicho ingrediente:
                     try {
@@ -83,13 +83,27 @@ public class ControladorModificarReceta implements Initializable {
                     String valorIngredienteSeleccionado = (String) ingredienteSeleccionado;
 
                     try {
-                        PreparedStatement datosRecogidosRecetas = conexion.prepareStatement ( "SELECT STOCK FROM INGREDIENTE WHERE NOMBRE = ?" );
-                        datosRecogidosRecetas.setString ( 1, valorIngredienteSeleccionado );
-                        ResultSet resultadoDatosRecetas = datosRecogidosRecetas.executeQuery ( );
-                        resultadoDatosRecetas.next ();
-                        TxtfldCantidadIngrediente.setText ( resultadoDatosRecetas.getString ( "STOCK" ) );
+                        PreparedStatement ordenIdentificarIngrediente = conexion.prepareStatement("SELECT ID FROM INGREDIENTES WHERE NOMBRE = ?");
+                        ordenIdentificarIngrediente.setString ( 1, valorIngredienteSeleccionado );
+                        ResultSet resultadoDatos = ordenIdentificarIngrediente.executeQuery ( );
+                        resultadoDatos.next ( );
+                        ID_ING = resultadoDatos.getInt ( "ID" );
+
+                        PreparedStatement ordenIdentificarProducto = conexion.prepareStatement("SELECT ID FROM PRODUCTOS WHERE NOMBRE = ?");
+                        ordenIdentificarProducto.setString ( 1, nombreReceta.getText());
+                        ResultSet resultadoIdentificarProducto = ordenIdentificarProducto.executeQuery ( );
+                        resultadoIdentificarProducto.next ( );
+                        ID_PR = resultadoIdentificarProducto.getInt ( "ID" );
+
+                        PreparedStatement ordenCantidadNecesariaIngredientes = conexion.prepareStatement ( "SELECT CANTIDAD FROM NECESITA WHERE ING_ID = ? AND PR_ID = ?");
+                        ordenCantidadNecesariaIngredientes.setInt ( 1, ID_ING );
+                        ordenCantidadNecesariaIngredientes.setInt ( 2, ID_PR );
+                        ResultSet resultadoCantidadIngredientes = ordenCantidadNecesariaIngredientes.executeQuery ( );
+                        resultadoCantidadIngredientes.next ( );
+                        txtfldCantidadIngrediente.setText(resultadoCantidadIngredientes.getString("CANTIDAD"));
+
                     } catch (SQLException ex) {
-                        throw new RuntimeException ( ex );
+                        throw  new RuntimeException ( ex );
                     }
                 }
             } );
@@ -112,15 +126,14 @@ public class ControladorModificarReceta implements Initializable {
 
     private void rellenarListaIngredientes() throws SQLException {
         listaIngredientes.getItems().removeAll(listaIngredientes.getItems());
+
         PreparedStatement aux1 = conexion.prepareStatement ( "SELECT ID FROM PRODUCTOS WHERE NOMBRE = ?");
         aux1.setString (1, nombreReceta.getText());
         ResultSet resultadoIngredientes = aux1.executeQuery();
         resultadoIngredientes.next();
         int IDproducto = resultadoIngredientes.getInt(1);
 
-
-
-        PreparedStatement recogidaNombresIngredientes = conexion.prepareStatement("SELECT I.NOMBRE,N.ING_ID,N.CANTIDAD FROM NECESITA AS N INNER JOIN INGREDIENTES AS I ON I.ID = N.ING_ID WHERE N.PR_ID = ?");
+        PreparedStatement recogidaNombresIngredientes = conexion.prepareStatement("SELECT I.NOMBRE,N.ING_ID,N.PR_ID FROM NECESITA AS N INNER JOIN INGREDIENTES AS I ON I.ID = N.ING_ID WHERE N.PR_ID = ?");
         recogidaNombresIngredientes.setInt ( 1, IDproducto );
 
         ResultSet resultadoNombresIngredientes = recogidaNombresIngredientes.executeQuery();
@@ -133,36 +146,5 @@ public class ControladorModificarReceta implements Initializable {
         }
     }
 
-    /**
-    private void mostrarObjetosPorPantalla(ListView<String> lista){
-        //PARA DETECTAR Y RESPONDER EN BASE A LOS OBJETOS SELECCIONADOS
-        lista.setOnMouseClicked(event -> {
-            // Obtener el artículo seleccionado
-            Object selectedItem = lista.getSelectionModel().getSelectedItem();
 
-            // Acceder a los valores del elemento seleccionado y visualizarlos
-            if (selectedItem != null) {
-                // Asumiendo que el Item seleccionado es del tipo String
-                String selectedValue = (String) selectedItem;
-
-                // Buscar dicho ingrediente:
-                try {
-                    PreparedStatement datosRecogidosRecetas = conexion.prepareStatement("SELECT * FROM PRODUCTOS WHERE NOMBRE = ?");
-                    datosRecogidosRecetas.setString(1, selectedValue);
-                    ResultSet resultadoDatos = datosRecogidosRecetas.executeQuery();
-                    resultadoDatos.next();
-                    nombreReceta.setText(resultadoDatos.getString("NOMBRE"));
-                    precioReceta.setText(resultadoDatos.getString("PRECIO"));
-
-                    // PARA VISUALIZAR LOS INGREDIENTES QUE QUIERO MODIFICAR
-                    PreparedStatement cantidadIngredientes = conexion.prepareStatement("SELECT NOMBRE,STOCK FROM INGREDIENTES");
-                    ResultSet resultadoCantidad = datosRecogidosRecetas.executeQuery();
-
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-    }
-    **/
 }
