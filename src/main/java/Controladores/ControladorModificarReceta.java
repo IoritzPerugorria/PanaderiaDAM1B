2fca37dc1bc1aeb31cb8148c70d777aea06a2eb0
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-05-26 14:10:48 +0200

    Reconstrucción ControladorModificarReceta.java. modificarReceta.fxml:Metodos Accesibilidad

--- src/main/java/Controladores/ControladorModificarReceta.java
+++ src/main/java/Controladores/ControladorModificarReceta.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
@@ -24,11 +23,11 @@ public class ControladorModificarReceta implements Initializable {
     private int ID_ING;
     private int ID_PR;
     Connection conexion;
-
-    public ControladorModificarReceta() {
+    public ControladorModificarReceta(){
         nombreReceta = new TextField();
         precioReceta = new TextField();
         txtfldCantidadIngrediente = new TextField();
+
     }
 
 
@@ -37,102 +36,131 @@ public class ControladorModificarReceta implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //PRIMERO DESHABILITAREMOS LOS NODOS HASTA QUE NO SE SELECCIONE UNA RECETA
+        this.deshabilitarNodosEscritura();
+
         //PARA MOSTRAR LAS RECETAS DE LA BASE DE DATOS
         try {
-            conexion = ConexionBBDD.conectar(conexion);
-            this.rellenarListaRecetas();
+            conexion = ConexionBBDD.conectar ( conexion );
+            this.rellenarListaRecetas ();
             //PARA DETECTAR Y RESPONDER EN BASE A LOS OBJETOS SELECCIONADOS
-            listaRecetas.setOnMouseClicked(event -> {
+            listaRecetas.setOnMouseClicked ( event -> {
                         // Obtener el artículo seleccionado
-                        Object recetaSeleccionada = listaRecetas.getSelectionModel().getSelectedItem();
+                        Object recetaSeleccionada = listaRecetas.getSelectionModel ( ).getSelectedItem ( );
                         // Acceder a los valores del elemento seleccionado y visualizarlos
-                        if (recetaSeleccionada != null) {
+                        if ( recetaSeleccionada != null ) {
                             // Asumiendo que el Item seleccionado es del tipo String
                             String valorRecetaSeleccionada = (String) recetaSeleccionada;
-                            txtfldCantidadIngrediente.setText("");
+
+
+                            /**
+                             AL SELECCIONAR UNA RECETA, HABILITAREMOS LOS NODOS "nombreReceta" Y "precioReceta",
+                             PERO DESACTIVAREMOS POR EN ESE MOMENTO EL NODO "txtfldCantidadIngrediente",
+                             PORQUE EN DICHA RECETA TODAVIA NO SE HA SELECCIONADO NINGUN INGREDIENTE
+                             **/
+                            this.habilitarNodosEscrituraNombrePrecio();
+                            this.deshabilitarNodoEscrituraIngredientes();
+
 
                             // Buscar dicho ingrediente:
                             try {
-                                PreparedStatement datosRecogidosRecetas = conexion.prepareStatement("SELECT * FROM PRODUCTOS WHERE NOMBRE = ?");
-                                datosRecogidosRecetas.setString(1, valorRecetaSeleccionada);
-                                ResultSet resultadoDatos = datosRecogidosRecetas.executeQuery();
-                                resultadoDatos.next();
-                                nombreReceta.setText(resultadoDatos.getString("NOMBRE"));
-                                precioReceta.setText(resultadoDatos.getString("PRECIO"));
+                                PreparedStatement datosRecogidosRecetas = conexion.prepareStatement ( "SELECT * FROM PRODUCTOS WHERE NOMBRE = ?" );
+                                datosRecogidosRecetas.setString ( 1, valorRecetaSeleccionada );
+                                ResultSet resultadoDatos = datosRecogidosRecetas.executeQuery ( );
+                                resultadoDatos.next ( );
+                                nombreReceta.setText ( resultadoDatos.getString ( "NOMBRE" ) );
+                                precioReceta.setText ( resultadoDatos.getString ( "PRECIO" ) );
                                 this.rellenarListaIngredientes();
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
+                            }
+                            catch (SQLException e) {
+                                throw new RuntimeException ( e );
                             }
                         }
                     }
             );
-            listaIngredientes.setOnMouseClicked(event -> {
-                Object ingredienteSeleccionado = listaIngredientes.getSelectionModel().getSelectedItem();
-                if (ingredienteSeleccionado != null) {
+            listaIngredientes.setOnMouseClicked ( event -> {
+                Object ingredienteSeleccionado = listaIngredientes.getSelectionModel ( ).getSelectedItem ( );
+
+                if ( ingredienteSeleccionado != null ) {
                     String valorIngredienteSeleccionado = (String) ingredienteSeleccionado;
+                    /** EL NODO "txtfldCantidadIngrediente" UNICAMENTE SE HABILITARÁ CUANDO SE HAYA SELECCIONADO UN INGREDIENTE **/
+                    this.habilitarNodoEscrituraIngredientes();
 
                     try {
                         PreparedStatement ordenIdentificarIngrediente = conexion.prepareStatement("SELECT ID FROM INGREDIENTES WHERE NOMBRE = ?");
-                        ordenIdentificarIngrediente.setString(1, valorIngredienteSeleccionado);
-                        ResultSet resultadoDatos = ordenIdentificarIngrediente.executeQuery();
-                        resultadoDatos.next();
-                        ID_ING = resultadoDatos.getInt("ID");
-
+                        ordenIdentificarIngrediente.setString ( 1, valorIngredienteSeleccionado );
+                        ResultSet resultadoDatos = ordenIdentificarIngrediente.executeQuery ( );
+                        resultadoDatos.next ( );
+                        ID_ING = resultadoDatos.getInt ( "ID" );
                         PreparedStatement ordenIdentificarProducto = conexion.prepareStatement("SELECT ID FROM PRODUCTOS WHERE NOMBRE = ?");
-                        ordenIdentificarProducto.setString(1, nombreReceta.getText());
-                        ResultSet resultadoIdentificarProducto = ordenIdentificarProducto.executeQuery();
-                        resultadoIdentificarProducto.next();
-                        ID_PR = resultadoIdentificarProducto.getInt("ID");
-
-                        PreparedStatement ordenCantidadNecesariaIngredientes = conexion.prepareStatement("SELECT CANTIDAD FROM NECESITA WHERE ING_ID = ? AND PR_ID = ?");
-                        ordenCantidadNecesariaIngredientes.setInt(1, ID_ING);
-                        ordenCantidadNecesariaIngredientes.setInt(2, ID_PR);
-                        ResultSet resultadoCantidadIngredientes = ordenCantidadNecesariaIngredientes.executeQuery();
-                        resultadoCantidadIngredientes.next();
+                        ordenIdentificarProducto.setString ( 1, nombreReceta.getText());
+                        ResultSet resultadoIdentificarProducto = ordenIdentificarProducto.executeQuery ( );
+                        resultadoIdentificarProducto.next ( );
+                        ID_PR = resultadoIdentificarProducto.getInt ( "ID" );
+                        PreparedStatement ordenCantidadNecesariaIngredientes = conexion.prepareStatement ( "SELECT CANTIDAD FROM NECESITA WHERE ING_ID = ? AND PR_ID = ?");
+                        ordenCantidadNecesariaIngredientes.setInt ( 1, ID_ING );
+                        ordenCantidadNecesariaIngredientes.setInt ( 2, ID_PR );
+                        ResultSet resultadoCantidadIngredientes = ordenCantidadNecesariaIngredientes.executeQuery ( );
+                        resultadoCantidadIngredientes.next ( );
                         txtfldCantidadIngrediente.setText(resultadoCantidadIngredientes.getString("CANTIDAD"));
-
                     } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
+                        throw  new RuntimeException ( ex );
                     }
                 }
-            });
+            } );
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException ( e );
         }
     }
-
     private void rellenarListaRecetas() throws SQLException {
         PreparedStatement recogidaNombresRecetas = conexion.prepareStatement("SELECT NOMBRE FROM PRODUCTOS ");
         ResultSet resultadoNombres = recogidaNombresRecetas.executeQuery();
         ArrayList<String> listaNombresRecetas = new ArrayList<>();
-        while (resultadoNombres.next()) {
+        while (resultadoNombres.next()){
             listaNombresRecetas.add(resultadoNombres.getString("NOMBRE"));
         }
         for (int i = 0; i < listaNombresRecetas.size(); i++) {
             listaRecetas.getItems().add(listaNombresRecetas.get(i));
         }
     }
-
     private void rellenarListaIngredientes() throws SQLException {
         listaIngredientes.getItems().removeAll(listaIngredientes.getItems());
-
-        PreparedStatement aux1 = conexion.prepareStatement("SELECT ID FROM PRODUCTOS WHERE NOMBRE = ?");
-        aux1.setString(1, nombreReceta.getText());
+        PreparedStatement aux1 = conexion.prepareStatement ( "SELECT ID FROM PRODUCTOS WHERE NOMBRE = ?");
+        aux1.setString (1, nombreReceta.getText());
         ResultSet resultadoIngredientes = aux1.executeQuery();
         resultadoIngredientes.next();
         int IDproducto = resultadoIngredientes.getInt(1);
-
         PreparedStatement recogidaNombresIngredientes = conexion.prepareStatement("SELECT I.NOMBRE,N.ING_ID,N.PR_ID FROM NECESITA AS N INNER JOIN INGREDIENTES AS I ON I.ID = N.ING_ID WHERE N.PR_ID = ?");
-        recogidaNombresIngredientes.setInt(1, IDproducto);
-
+        recogidaNombresIngredientes.setInt ( 1, IDproducto );
         ResultSet resultadoNombresIngredientes = recogidaNombresIngredientes.executeQuery();
         ArrayList<String> listaNombresIngredientes = new ArrayList<>();
-        while (resultadoNombresIngredientes.next()) {
+        while (resultadoNombresIngredientes.next()){
             listaNombresIngredientes.add(resultadoNombresIngredientes.getString("I.NOMBRE"));
         }
         for (String listaNombresIngrediente : listaNombresIngredientes) {
             listaIngredientes.getItems().add(listaNombresIngrediente);
         }
+    }
+
+    // METODOS RELACIONADOS CON LA ACCESIBILIDAD DE NODOS
+    private void deshabilitarNodosEscritura(){
+        nombreReceta.setDisable(true);
+        precioReceta.setDisable(true);
+        txtfldCantidadIngrediente.setDisable(true);
+    }
+
+    private void habilitarNodosEscrituraNombrePrecio(){
+        nombreReceta.setDisable(false);
+        precioReceta.setDisable(false);
+    }
+
+    private void deshabilitarNodoEscrituraIngredientes(){
+        txtfldCantidadIngrediente.setDisable(true);
+        txtfldCantidadIngrediente.setText("");
+    }
+
+    private void habilitarNodoEscrituraIngredientes(){
+        txtfldCantidadIngrediente.setDisable(false);
     }
 
 
